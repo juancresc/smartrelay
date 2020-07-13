657eb35464b98ad40feefbc1f769502bb1e7b467
juancrescente <juan.crescente@gmail.com>
2020-07-12 21:41:34 -0300

    init

--- alarms/logic.py
+++ alarms/logic.py
@@ -5,11 +5,20 @@ class CalculateCurrentStatus():
 
     def execute(self):
         now = datetime.now().time()
-        print(now)
+        
+        #prev alarm, any previous at now
         closest_alarm = Alarm.objects.filter(alarm_time__lte=now)
         closest_alarm = closest_alarm.order_by('-alarm_time')
         closest_alarm = closest_alarm.first()
-        print(closest_alarm)
         if closest_alarm:
             return closest_alarm.action
+
+        #latest alarm, the one that is more late
+        closest_alarm = Alarm.objects.all()
+        closest_alarm = closest_alarm.order_by('-alarm_time')
+        closest_alarm = closest_alarm.first()
+        if closest_alarm:
+            return closest_alarm.action
+
+        #no alarm, keep it off
         return False
