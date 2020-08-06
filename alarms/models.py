780fd99b140be0f40fe94e66c23d77da8b3f4c40
juancrescente <juan.crescente@gmail.com>
2020-08-06 18:24:46 -0300

    init

--- alarms/models.py
+++ alarms/models.py
@@ -5,6 +5,16 @@ from django.utils.timezone import now, timedelta
 class Alarm(models.Model):
     alarm_time = models.TimeField()
     action = models.BooleanField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        action = "ON" if self.action else "OFF"
+        return "{} at {} ".format(action, self.alarm_time)
+
+
+class OnOff(models.Model):
+    action = models.BooleanField()
+    updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         action = "ON" if self.action else "OFF"
